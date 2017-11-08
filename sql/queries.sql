af330de411e7ae00b9ad7cdc267fa9966df914d2
Stefan Ziegler <edi.gonzales@gmail.com>
2017-11-08 16:17:28 +0100

    wip

--- sql/queries.sql
+++ sql/queries.sql
@@ -11,13 +11,16 @@ FROM
     arp_npl.rechtsvorschrften_hinweisweiteredokumente  
 */    
     
-WITH RECURSIVE subordinates(col1, col2) AS
+WITH RECURSIVE subordinates AS
 (
     SELECT
         t_id,
-        ursprung AS urursprung,
         ursprung,
-        hinweis
+        hinweis,
+        NULL::text[] AS full_name
+        --array_agg(ursprung) AS foo
+        --ARRAY[ursprung, hinweis]::text[] AS full_name
+        --ursprung::text AS ursprung_full_name
     FROM
         arp_npl.rechtsvorschrften_hinweisweiteredokumente
     --WHERE
@@ -27,21 +30,49 @@ WITH RECURSIVE subordinates(col1, col2) AS
     
     SELECT
         e.t_id, 
-        s.hinweis AS urursprung,
         e.ursprung,
-        e.hinweis
+        e.hinweis,
+        array_append(full_name, s.ursprung::text)
+        --array_agg(e.ursprung)
+        --array_append(array_append(full_name, s.hinweis::text), e.hinweis::text)
+        --array_append(full_name, s.hinweis::text)
+        --(s.ursprung_full_name || '->' || e.ursprung::text) AS ursprung_full_name
     FROM
         arp_npl.rechtsvorschrften_hinweisweiteredokumente AS e
     INNER JOIN
         subordinates s ON s.hinweis = e.ursprung
 
+),
+foo AS 
+(
+    SELECT 
+        *,
+        array_append(array_append(full_name, ursprung::text), hinweis::text) AS my_array
+    FROM
+        subordinates
+    ORDER BY
+        ursprung
 )
-SELECT 
-    *
+SELECT
+  --t_id,
+    --array_length(my_array, 1),
+    --my_array[1],
+    unnest(my_array)
+    
+    
 FROM
-    subordinates
-
+    foo
+--GROUP BY
+  --  my_array[1],
 /*
+    
+SELECT 
+    a.ursprung, a.hinweis
+FROM
+    arp_npl.rechtsvorschrften_hinweisweiteredokumente AS a
+    JOIN arp_npl.rechtsvorschrften_hinweisweiteredokumente AS b 
+    ON a.ursprung = b.hinweis
+
 ,
 urursprung AS 
 (
@@ -66,7 +97,5 @@ FROM
     ON s.ursprung = u.ursprung
 ORDER BY
     s.ursprung
-    
-    
-
+   
 */
